586eb3fb635c5176a69b4f61f0f2f843a215af28
肖勖 <xiao@xiaoxudeMacBook-Pro.local>
2019-08-05 22:41:50 +0800

    all

---
+++ lesson/src/lesson1/Test1.java
@@ -0,0 +1,7 @@
+package lesson1;
+
+public class Test1 {
+    public static void main(String[] args) {
+        System.out.println("hello, he~");
+    }
+}
---
+++ lesson/src/lesson2/Test2.java
@@ -0,0 +1,31 @@
+package lesson2;
+
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+public class Test2 {
+    public static void main(String[] args) {
+        ExecutorService executorService = Executors.newFixedThreadPool(10);
+
+        for (int i = 0; i < 10; i++) {
+            Future<?> submit = executorService.submit(new Runnable() {
+                @Override
+                public void run() {
+                    System.out.println(Single.getSingle());
+                }
+            });
+            submit.isDone();
+            try {
+                submit.get();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } catch (ExecutionException e) {
+                e.printStackTrace();
+            }
+        }
+        System.out.println("result");
+        executorService.shutdown();
+    }
+}
