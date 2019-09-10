742eef25da8bb636879931dc04019158c33ef063
肖勖 <xiao@xiaoxudeMacBook-Pro.local>
2019-09-10 11:25:54 +0800

    LC23

---
+++ lesson/src/LeetCode/High/LC23.java
@@ -0,0 +1,23 @@
+package LeetCode.High;
+
+public class LC23 {
+
+    public static void main(String[] args) {
+
+    }
+
+    public ListNode mergeKLists(ListNode[] lists) {
+
+
+        return null;
+    }
+
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+}
